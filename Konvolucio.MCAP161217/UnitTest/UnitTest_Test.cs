3db8ba2eba7f7b8bbf4123468ca0f8913441f7ee
marrob <marrob50hz@gmail.com>
2017-01-19 15:36:02 +0100

    Signal Generation added...

--- Konvolucio.MCAP161217/UnitTest/UnitTest_Test.cs
+++ Konvolucio.MCAP161217/UnitTest/UnitTest_Test.cs
@@ -7,9 +7,11 @@ namespace Konvolucio.MCAP161217.UnitTest
     using System.IO;
     using System.Linq;
     using System.Net.Configuration;
+    using System.Net.Mail;
     using System.Text;
     using NUnit.Framework;
     using System.Numerics;
+    using System.Threading;
     using NUnit.Framework.Constraints;
 
     [TestFixture]
@@ -110,35 +112,48 @@ namespace Konvolucio.MCAP161217.UnitTest
         }
 
 
-        public void DftBruteForce(double[] signal)
-        {
-            // time and frequency domain data arrays
-            int n, k, N;            // indices for time and frequency domains
-            double[] Xre, Xim;      // DFT of x (real and imaginary parts)
-            double[] P;             // power spectrum of x
-
-            N = signal.Length;
-            Xre = new double[N];
-            Xim = new double[N];
-            P = new double[N];
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="signal"></param>
+        /// <returns></returns>
+        public Complex[] DftBruteForce(double[] signal)
+        {       
+            var N = signal.Length;
+            var Xre = new double[N];
+            var Xim = new double[N];
+       
+            var complexArray = new Complex[N];
 
             // Calculate DFT of x using brute force
-            for (k = 0; k < N; ++k)
+            for (int k = 0; k < N; ++k)
             {
                 // Real part of X[k]
                 Xre[k] = 0;
-                for (n = 0; n < N; ++n) Xre[k] += signal[n] * Math.Cos(n * k * 2 * Math.PI / N);
+                for (int n = 0; n < N; ++n) Xre[k] += signal[n] * Math.Cos(n * k * 2 * Math.PI / N);
 
                 // Imaginary part of X[k]
                 Xim[k] = 0;
-                for (n = 0; n < N; ++n) Xim[k] -= signal[n] * Math.Sin(n * k * 2 * Math.PI / N);
+                for (int n = 0; n < N; ++n) Xim[k] -= signal[n] * Math.Sin(n * k * 2 * Math.PI / N);
 
-                // Power at kth frequency bin
-                P[k] = Xre[k] * Xre[k] + Xim[k] * Xim[k];
+                complexArray[k] = new Complex(Xre[k], Xim[k]);
             }
-
-            DoubleArrayToFile(P, "D:\\power2.csv");
+            return complexArray;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private double[] GetPowerSpectrum(Complex[] complexSignal)
+        {
+            double[] spectrum = new double[complexSignal.Length];
+            var index = 0;
+            foreach (var signal in complexSignal)
+                spectrum[index++] = Complex.Abs(signal);
+            return spectrum;
+        }
+
 
         [Test]
         public void LoadTestVector()
@@ -170,19 +185,24 @@ namespace Konvolucio.MCAP161217.UnitTest
         }
 
 
-
+        /// <summary>
+        /// 
+        /// </summary>
         [Test]
         public void CreateTest0002()
         {
-           var signal =  SequenceGenerator( amplitude: 1,
-                                             cycleNum: 1,
-                                             resolution: Math.PI/1000,
+           var signal =  SequenceGenerator(  amplitude: 1,
+                                             cycleNum: 100,
+                                             resolution: Math.PI/10,
                                              phase: Math.PI / 2,
                                              offset: 0);
 
             DoubleArrayToFile(signal, "D:\\tesztvector.csv");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         [Test]
         public void CreateTest0003()
         {
@@ -211,15 +231,90 @@ namespace Konvolucio.MCAP161217.UnitTest
                                                     double phase,
                                                     double offset)
         {
-            double[] squence = new double[0];
+            double[] sequence = new double[0];
             int index = 0;
             for (int cycle = 0; cycle < cycleNum; cycle++)
                 for (double p = 0; p <= (2 * Math.PI); p += resolution)
                 {
-                    Array.Resize(ref squence, index + 1);
-                    squence[index++] = amplitude * Math.Sin(p + phase) + offset;   
+                    Array.Resize(ref sequence, index + 1);
+                    sequence[index++] = amplitude * Math.Sin(p + phase) + offset;   
                 }
 
+            return sequence;
+        }
+
+
+        [Test]
+        public void SequnceGeneratorTest1()
+        {
+            var signal = SequnceGenerator(
+                    samplingRate: 2000, // 2000Sample/sec
+                    frequency: 10, //Hz
+                    samples: 1000, //data record length in ms
+                    amplitude: 1, //V
+                    offset: 0, //V
+                    phase: 0 //radian
+                );
+
+            DoubleArrayToFile(signal, "D:\\tesztvector.csv");
+        }
+
+        [Test]
+        public void SequnceGeneratorTest2()
+        {
+            var signal = SequnceGenerator(
+                    samplingRate: 2000, // 2000Sample/sec 
+                    frequency: 100, //Hz100
+                    samples: 1000, //data record length in ms
+                    amplitude: 1, //V
+                    offset: 0, //V
+                    phase: 0 //radian
+                );
+
+            DoubleArrayToFile(signal, "D:\\signal100Hz2000Sample.csv");
+
+            var complexSignal =  DftBruteForce(signal);
+
+            DoubleArrayToFile(GetPowerSpectrum(complexSignal), "D:\\PowerSectrum.csv");
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="samplingRate">Sample Rate in Sample/Sec eg.:2000</param>
+        /// <param name="frequency">Signal Frequency in Hz. eg.: 10Hz </param>
+        /// <param name="samples"></param>
+        /// <param name="amplitude">Signal Amplitudo in V. eg.: 1V </param>
+        /// <param name="offset">Signal offset in V. eg.: 1V</param>
+        /// <param name="phase">Signal phase in rad. egy Math.PI/4 </param>
+        /// <returns></returns>
+        public static double[] SequnceGenerator(
+                int samplingRate,
+                int frequency,
+                int samples,
+                double amplitude,
+                double offset,
+                double phase
+            )
+        {
+            double[] squence = new double[0];
+
+            var sampleTime = 1000.0/samplingRate; //ms
+            var periodTime = 1000.0/frequency;  //ms
+            var periodSamples = periodTime/sampleTime; //count
+
+            var sample = 0;
+            do
+            {
+                for (double p = 0; p <= (2*Math.PI); p += (2*Math.PI)/periodSamples)
+                {
+                    Array.Resize(ref squence, sample + 1);
+                    squence[sample++] = amplitude*Math.Sin(p + phase) + offset;
+                    if (sample >= samples)
+                        break;
+                }
+            } while (sample < samples); 
             return squence;
         }
 
@@ -262,11 +357,19 @@ namespace Konvolucio.MCAP161217.UnitTest
             var f = new FileInfo(path);
             if (f.Exists)
                 f.Delete();
+            using (var sw = new StreamWriter(path))
+            {
+                foreach (var value in data)
+                    sw.WriteLine(value.ToString());
+            }
+        }
 
-            var sw = new StreamWriter(path);
-            foreach (var value in data)
-                sw.WriteLine(value.ToString());
-            
+
+        private class Waveform
+        {
+            public DateTime Timestamp { get; set; }
+            public double DeltaX { get; set; }
+            public double[] YArray { get; set; }
         }
 
     }
